25023a802221d20a32f12cbc10eb8a0f1b4a20ba
Ruben Aalders <Ruben Aalders>
2017-02-23 14:57:58 +0100

    Default setup Beacon and BeaconManager

---
+++ Physical Web HoloLens/Assets/Scripts/Beacon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beacon : MonoBehaviour {
+    string address { get; set; }
+    List<int> strength { get; set; }
+    string output { get; set; }
+
+    public Beacon(string address, int strength, string output)
+    {
+        this.address = address;
+        this.strength.Add(strength);
+        this.output = output;
+    }
+
+    public string getAddress()
+    {
+        return address;
+    }
+
+    public void addStrength(int strength)
+    {
+        this.strength.Add(strength);
+    }
+}
---
+++ Physical Web HoloLens/Assets/Scripts/BeaconManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaconManager : MonoBehaviour {
+    List<Beacon> beacons { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        beacons = new List<Beacon>();
+    }
+
+    /// <summary>
+    /// Update the beacon list
+    /// If the beacon does not exist yet, add a new one
+    /// Else add an updated strength value to the beacon
+    /// </summary>
+    /// <param name="address">Unique bluetooth address</param>
+    /// <param name="strength">Signal strength</param>
+    /// <param name="output">Eddystone URL</param>
+    void  Update(string address, int strength, string output)
+    {
+        // If beacon does not exist yet, create a new one
+        if(!existInList(address))
+        {                                      
+            Beacon beacon = new Beacon(address, strength, output);
+            beacons.Add(beacon);
+        } else // Beacon already exists. Add an updated strength value to it
+        {
+            Beacon beacon = getBeacon(address);
+            beacon.addStrength(strength);
+        }
+    }
+
+    /// <summary>
+    /// Check if the beacon already exists in the list with beacons
+    /// </summary>
+    /// <param name="address">Unique bluetooth address</param>
+    /// <returns>If the beacon already exists in the list of beacons</returns>
+    bool existInList(string address)
+    {
+        foreach(Beacon beacon in beacons)
+        {
+            if (beacon.getAddress().Equals(address))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Get the beacon for the given unique bluetooth address
+    /// Beacon should already be in the list
+    /// </summary>
+    /// <param name="address">Unique bluetooth address</param>
+    /// <returns>Beacon data that belongs to the given address else null</returns>
+    Beacon getBeacon(string address)
+    {
+        foreach (Beacon beacon in beacons)
+        {
+            if (beacon.getAddress().Equals(address))
+            {
+                return beacon;
+            }
+        }
+
+        return null;
+    }
+}
